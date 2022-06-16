d5a2183b39db5a29a3cda3b677fd7dcb0860d719
MaverickSamurai <mirsamirba@code.edu.az>
2022-06-17 02:09:07 +0400

    HomeWork-Introdaction

---
+++ HomeWork-Introdaction/HomeWork-Introdaction/Program.cs
@@ -0,0 +1,61 @@
+﻿//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+//int n = 21;
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("dividable");
+//}
+
+
+//else
+
+
+//    Console.WriteLine("not dividable");
+
+//2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+//int n = 36, m=24 ;
+//if (n % 2 == 0 && m % 2 == 0)
+//{ Console.WriteLine(n + m); }
+//else
+//    Console.WriteLine("n and m not even numbers");
+
+
+//3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+
+//int n = 3;
+//int m = 49;
+//int sum = 0;
+
+//for (int i = 0; i < m; i++)
+
+//if (i % 2 == 1 && i > n)
+//sum += i;
+
+//{ Console.WriteLine(sum); }
+
+
+
+
+
+//4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int[] arr = { 3, 5, 7, 24, 35, 74, 99 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//    if (arr[i] % 2 == 1)
+//        sum = sum + arr[i];
+//Console.WriteLine(sum);
+
+//5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+
+
+//int[] arr = { 5, 6, 11, 19, 23, 39, 44, 56, 68, 76 };
+//int count = 0;
+//for (int i = 0; i < arr.Length; i++)
+//    if (arr[i] % 2 == 0)
+//        count++;
+//Console.WriteLine(count);
